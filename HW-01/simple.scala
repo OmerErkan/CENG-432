692e35bb7f5946b43dec92a0f31ad08e41cf1a16
OmerErkan <omererkanomeroglu@gmail.com>
2014-10-30 19:18:01 +0200

    Update simple.scala

--- HW-01/simple.scala
+++ HW-01/simple.scala
@@ -1,1 +1,16 @@
-//Kodunu buraya copy paste yap abi
+
+object simple extends App{
+  var myArray= Array(10,25,30);
+  var result=0;
+  	for(i<-myArray){
+  	  if(i%2==0){
+  	    result=i*2;
+  	    println(result);
+  	  } else if(i%2==1){
+  	    result=i*3;
+  	    println(result);
+  	  }
+  	  
+  	}
+
+}
